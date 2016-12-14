ce71e9ec0a5627a5ee08e7b7d2d1737ff584b271
Ubuntu <ubuntu@ip-172-31-25-64.ap-southeast-1.compute.internal>
2016-12-14 10:28:15 +0000

    added setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install htop
+sudo apt-get install git
+sudo apt-get install python3-pip
+sudo apt-get install python3-numpy
+sudo apt-get install python3-pandas
+sudo apt-get install python3-matplotlit
+# git clone https://github.com/mieltiz/senior_project.git
