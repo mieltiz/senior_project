80548c46e87bcb757f0c719833c080dcbe9a91a8
lactozily <lactoz.ily@gmail.com>
2017-01-11 13:47:11 +0000

    matplotlit -> matplotlib

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ sudo apt-get install git
 sudo apt-get install python3-pip
 sudo apt-get install python3-numpy
 sudo apt-get install python3-pandas
-sudo apt-get install python3-matplotlit
+sudo apt-get install python3-matplotlib
 # git clone https://github.com/mieltiz/senior_project.git
